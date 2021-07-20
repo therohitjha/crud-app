b11d42ada22de4b2b271d9d91361672b123176b3
Rohit Jha <therohitjha@gmail.com>
2021-07-20 12:30:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,119 @@
-import {useState} from 'react';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
 
-export default function App(){
-  const [firstCount, setFirstCount] = useState(0);
-  const [secondCount, setSecondCount] = useState(0);
-  const handleClick = () => {
-    setFirstCount((count) => count + 1); // No re-render yet
-    setSecondCount((count) => count + 0.5); // No re-render yet
-    // Re-render (updates batched)
-  };
-  /*
-  const alternativeHandleClick = () => {
-    Promise.resolve().then(() => {
-      setFirstCount((count) => count + 1); // Re-render
-      setSecondCount((count) => count + 0.5); // Re-render
+export default function App() {
+  const [data, setData] = useState("");
+  const [db, setDB] = useState([]);
+  const [dublicates, setDublicates] = useState(false);
+  const [update, setUpdate] = useState(false);
+  const [deleteIndex, setDeleteIndex] = useState(null);
+  const focus = useRef(null);
+
+  useEffect(() => {
+    focus.current.focus();
+    setTimeout(() => {
+      setDublicates(false);
+    }, 5000);
+  }, [dublicates]);
+
+  const handleSubmit = () => {
+    setDB((prev) => {
+      if (prev.some((e) => e === data)) {
+        setDublicates(true);
+        return [...prev];
+      }
+      return [...prev, data];
     });
+    setData("");
   };
-  */
+  const handleEdit = (e, i) => {
+    db.forEach((item) => {
+      if (item === e) {
+        setData(item);
+        setDeleteIndex(i);
+      }
+    });
+    setUpdate(true);
+  };
+  const handleUpdate = () => {
+    const item = [...db];
+    item.splice(deleteIndex, 1, data);
+    setDB(item);
+    setUpdate(false);
+    setData("");
+    setDeleteIndex(null);
+  };
+  const handleDelete = (i) => {
+    const item = [...db];
+    item.splice(i, 1);
+    setDB(item);
+    setUpdate(false);
+    setData("");
+  };
 
   return (
-    <div>
-      <button onClick={handleClick}>Next</button>
-      <span
-        style={{
-          color: firstCount % 2 === 0 ? "blue" : "black",
-        }}
-      >
-        {secondCount}
-      </span>
+    <div className="App">
+      <h1>React CRUD Operations</h1>
+      <input
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+        ref={focus}
+      />
+      {update ? (
+        <button onClick={handleUpdate}>Update</button>
+      ) : (
+        <button onClick={handleSubmit}>Submit</button>
+      )}
+      {dublicates && (
+        <>
+          <br />
+          <span style={{ color: "red" }}>Dublicates are not allowed</span>
+        </>
+      )}
+      <br />
+      {db?.length ? (
+        <table className="tableContainer">
+          <tr className="flex">
+            <th style={{ fontWeight: "700" }}>List</th>
+            <th
+              style={{
+                color: "green",
+                fontWeight: "700",
+              }}
+            >
+              Edit
+            </th>
+            <th style={{ color: "red", fontWeight: "700" }}>Delete</th>
+          </tr>
+          {db?.map((item, i) => {
+            return (
+              <tr key={item} className="flex">
+                <td>{item}</td>
+                <td
+                  style={{ cursor: "pointer", marginTop: "5px" }}
+                  onClick={(e) => {
+                    handleEdit(item, i);
+                    e.stopPropagation();
+                  }}
+                >
+                  Edit
+                </td>
+                <td
+                  style={{ cursor: "pointer", marginTop: "5px" }}
+                  onClick={(e) => {
+                    handleDelete(i);
+                    e.stopPropagation();
+                  }}
+                >
+                  Delete
+                </td>
+              </tr>
+            );
+          })}{" "}
+        </table>
+      ) : (
+        <div className="empty">List is Empty</div>
+      )}
     </div>
   );
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import App from "./App";
-const container = document.getElementById("app");
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<React.StrictMode><App /></React.StrictMode>);
