9cfac24823132d18bba67a15c97c54962eeed00b
Rohit Jha <therohitjha@gmail.com>
2021-07-28 00:13:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 
 export default function App() {
   const [data, setData] = useState("");
-  const [db, setDB] = useState([]);
+  const [db, setDB] = useState(() =>
+  JSON.parse(localStorage.getItem("db"))
+    ? JSON.parse(localStorage.getItem("db"))
+    : []);
   const [dublicates, setDublicates] = useState(false);
   const [update, setUpdate] = useState(false);
   const [deleteIndex, setDeleteIndex] = useState(null);
@@ -15,15 +18,34 @@ export default function App() {
   }, [dublicates]);
 
   const handleSubmit = () => {
-    setDB((prev) => {
-      if (prev.some((e) => e === data)) {
-        setDublicates(true);
-        return [...prev];
-      }
-      return [...prev, data];
-    });
-    setData("");
+    if (data) {
+      setDB((prev) => {
+        if (prev.some((e) => e === data)) {
+          setDublicates(true);
+          return [...prev];
+        }
+        localStorage.setItem("db", JSON.stringify([...prev, data]));
+        return [...prev, data];
+      });
+      setData("");
+    }
   };
+
+  const handleEnterKeyPress = (event) => {
+    if (data && event.charCode === 13) {
+      setDB((prev) => {
+        if (prev.some((e) => e === data)) {
+          setDublicates(true);
+          return [...prev];
+        }
+        localStorage.setItem("db", JSON.stringify([...prev, data]));
+        return [...prev, data];
+      });
+      setData("");
+    }
+  };
+
+  
   const handleEdit = (e, i) => {
     db.forEach((item) => {
       if (item === e) {
@@ -33,27 +55,33 @@ export default function App() {
     });
     setUpdate(true);
   };
+
+
   const handleUpdate = () => {
-    const item = [...db];
-    if(item.some(e=>e===data)){
+    const item =[...db];
+    if (item.some((e) => e === data)) {
       setDublicates(true);
-    }
-    else{
-    item.splice(deleteIndex, 1, data);
-    setDB(item);
-    setUpdate(false);
-    setData("");
-    setDeleteIndex(null);
+    } else {
+      item.splice(deleteIndex, 1, data);
+      localStorage.setItem("db", JSON.stringify(item));
+      setDB(item);
+      setUpdate(false);
+      setData("");
+      setDeleteIndex(null);
     }
   };
+
+
   const handleDelete = (i) => {
     const item = [...db];
     item.splice(i, 1);
+    localStorage.setItem("db", JSON.stringify(item));
     setDB(item);
     setUpdate(false);
     setData("");
   };
-
+  // localStorage.clear();
+  console.log(db);
   return (
     <div className="App">
       <h1>React CRUD Operations</h1>
@@ -61,6 +89,7 @@ export default function App() {
         value={data}
         onChange={(e) => setData(e.target.value)}
         autoFocus
+        onKeyPress={handleEnterKeyPress}
       />
       {update ? (
         <button onClick={handleUpdate}>Update</button>
@@ -76,7 +105,10 @@ export default function App() {
       <br />
       {db?.length ? (
         <table className="tableContainer">
-          <thead className="flex" style={{padding:'.5em',background:'cyan',marginTop:'5px'}}>
+          <thead
+            className="flex"
+            style={{ padding: ".5em", background: "cyan", marginTop: "5px" }}
+          >
             <tr style={{ fontWeight: "700" }}>List</tr>
             <tr
               style={{
@@ -88,31 +120,32 @@ export default function App() {
             </tr>
             <tr style={{ color: "red", fontWeight: "700" }}>Delete</tr>
           </thead>
-          {db?.map((item, i) => {
-            return (
-              <tr key={item} className="flex list">
-                <td>{item}</td>
-                <td
-                  style={{ cursor: "pointer"}}
-                  onClick={(e) => {
-                    handleEdit(item, i);
-                    e.stopPropagation();
-                  }}
-                >
-                  Edit
-                </td>
-                <td
-                  style={{ cursor: "pointer"}}
-                  onClick={(e) => {
-                    handleDelete(i);
-                    e.stopPropagation();
-                  }}
-                >
-                  Delete
-                </td>
-              </tr>
-            );
-          })}{" "}
+          {db?.length &&
+           db?.map((item, i) => {
+              return (
+                <tr key={item} className="flex list">
+                  <td>{item}</td>
+                  <td
+                    style={{ cursor: "pointer" }}
+                    onClick={(e) => {
+                      handleEdit(item, i);
+                      e.stopPropagation();
+                    }}
+                  >
+                    Edit
+                  </td>
+                  <td
+                    style={{ cursor: "pointer" }}
+                    onClick={(e) => {
+                      handleDelete(i);
+                      e.stopPropagation();
+                    }}
+                  >
+                    Delete
+                  </td>
+                </tr>
+              );
+            })}{" "}
         </table>
       ) : (
         <div className="empty">List is Empty</div>
